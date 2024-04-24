2b68611fd88896623404b772c0f74bea51099d4a
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-04-24 16:03:29 +0000

    eslint fixes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,47 +1,47 @@
 export default class HolbertonCourse {
   constructor(name, lenght, students) {
-    if (typeof name != 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
-    if (typeof lenght != 'number') {
+    if (typeof lenght !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._lenght = lenght;
-    if (typeof students != 'object') {
+    if (typeof students !== 'object') {
       throw new TypeError('Students must be a array');
     }
     this._students = students;
-  };
+  }
 
   get name() {
     return this._name;
-  };
+  }
   set name(name) {
-    if (typeof name != 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
-  };
+  }
 
   get lenght() {
     return this._lenght;
-  };
+  }
   set lenght(lenght) {
-    if (typeof lenght != 'number') {
+    if (typeof lenght !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._lenght = lenght;
-  };
+  }
 
   get students() {
     return this._students;
-  };
+  }
   set students(students) {
-    if (typeof students != 'object') {
+    if (typeof students !== 'object') {
       throw new TypeError('Students must be a array');
     }
     this._students = students;
-  };
+  }
 }
   
