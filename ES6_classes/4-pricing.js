d266ea5b492477b0f52a9932678690fde27dd367
jotalgs <jose.pgilsuarez@gmail.com>
2024-04-26 12:39:53 -0300

    3y4 ; '

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -12,8 +12,8 @@ export default class Pricing {
 
   set amount(value) {
     if (typeof value !== 'number') {
-        throw new TypeError('Amount must be a number');
-      }
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = value;
   }
 
@@ -32,7 +32,7 @@ export default class Pricing {
     return `${this._amount} ${this._currency.name} (${this._currency.code}).`;
   }
 
-  convertPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
