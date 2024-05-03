5e54630b76a68131cdc87ed36b7ace4de43c89aa
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-05-03 16:01:24 -0300

    sdsad

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,27 +2,24 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName).catch((error) => error),
-  ])
-    .then(([userResp, fileResp]) => {
-      return [
-        {
-          status: "fulfilled",
-          value: userResp
-        },
-        {
-          status: "fulfilled",
-          value: fileResp
-        },
-      ];
-      }).catch((error) => {
-        return [
-          {
-            status: "rejected",
-            value: error
-          },
-        ];
-      });
+  const userResponse = signUpUser(firstName, lastName)
+    .then((value) => ({
+        status: 'fulfilled',
+        value,
+    }))
+    .catch((err) => ({
+      status: 'rejected',
+      value: err,    
+    }));
+  const fileResponse = uploadPhoto(fileName)
+    .then((value) => ({
+        status: 'fulfilled',
+        value,
+    }))
+    .catch((err) => ({
+      status: 'rejected',
+      value: err,    
+    }));
+  const response = [userResponse, fileResponse];
+  return Promise.allSettled(response);
 }
