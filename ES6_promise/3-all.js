fbc3d4794b6650ea3b4bad887c4653307d286146
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-05-03 15:37:37 -0300

    sdsad

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,9 +3,9 @@ const { uploadPhoto, createUser } = require('./utils.js');
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((resp) => {
-      const body = resp[0];
-      const firstName = resp[1];
-      const lastName = resp[1];
+      const { body } = resp[0];
+      const { firstName } = resp[1];
+      const { lastName } = resp[1];
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => console.error('Signup system offline'));
