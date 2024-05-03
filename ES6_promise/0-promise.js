eeb6055f354c3b56728d3422d12f86595090b03d
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-05-02 21:52:08 -0300

    0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
 return new Promise((resolve) => {
   setTimeout(() => {
-    const response = 'I promise'
+    const response = 'ok'
     resolve(response);
   }, 1000);
   });
