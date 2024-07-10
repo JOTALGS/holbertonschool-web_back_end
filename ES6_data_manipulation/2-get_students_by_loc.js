e2dc5450a417aa905a7c389238ee07a1fc949d91
jotalgs <jose.pgilsuarez@gmail.com>
2024-07-10 16:21:13 +0000

    lint 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(arrayOfStudents, city) {
-    return arrayOfStudents.filter((student) => student.location === city);
+  return arrayOfStudents.filter((student) => student.location === city);
 }
